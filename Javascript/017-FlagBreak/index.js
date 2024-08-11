84639383bf21064129dd76f3f7de2427f7cae740
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-08-12 00:35:13 +0500

    done with flage and

---
+++ Javascript/017-FlagBreak/index.js
@@ -0,0 +1,39 @@
+// CHAPTER-19: for loops:Flags, Booleans, array length, and loopus interruptus
+
+// flag:
+// Flag variable is used as a signal in programming to var the program know that a
+// certain condition has met. It usually acts as a boolean variable indicating a condition
+// to be either true or false
+
+// booleans
+// data type, 0s and 1s
+
+// array length
+// number of elements in an array
+
+// loopus interruptus
+// to break a loop before it's compvarion, meeting a condition
+
+// implementation
+// var cleanestCities = ["Peshawar", "Islamabad", "Karachi", "Lahore", "Multan"];
+// var cityToCheck = prompt("Please enter your city name: ");
+
+// flag
+// var cityMili = false;
+
+// for (var cityIndex = 0; cityIndex < cleanestCities.length; cityIndex++) {
+//   if (cityToCheck === cleanestCities[cityIndex]) {
+//     cityMili = true;
+
+// // loopus intrruptus
+// // break;
+// if (cityMili) {
+//     alert("It's one of the cleanest cities");
+//   } else {
+//     alert("It's not on the list");
+//   }
+  
+// }
+// }
+
+
